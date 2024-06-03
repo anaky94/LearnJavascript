ade0a117bbadea5c91338600ed7d295174767d17
anaky94 <anaky.44@gmail.com>
2024-06-03 21:30:17 +0200

    exo

--- apprendre_Javascript/javasxript.js
+++ apprendre_Javascript/javasxript.js
@@ -1,16 +1,28 @@
 // document.write("bonjour");
-
-// 
 // "%" modulo reste de la division 
 // 
-
-
-
 //  operateur d'affectation 
 
+/*
+confirm("souhaitez vous confirmez ?");
 
+if (confirm("souhaitez vous confirmez ?")){
+    // yes 
+    alert("ok");
+}
+else{
+    // non 
+    alert(" Annulation");
+}
+*/
 
+/*
+const age = prompt ("entré votre age ");
+const nom = prompt ("entré votre nom ");
 
+alert("vous avez " + age + " et vous ëtes " + nom );
+*/
 
-
-
+function dirBonjour(){
+const nom = prompt ("entré votre nom ");    
+}
