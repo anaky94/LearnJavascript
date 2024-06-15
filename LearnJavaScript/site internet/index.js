774eb6de1782fa5089c8793732ca2bf60717488d
anaky94 <82404924+anaky94@users.noreply.github.com>
2024-06-15 19:31:52 +0200

    Add files via upload

---
+++ LearnJavaScript/site internet/index.js
@@ -0,0 +1,45 @@
+
+/* documentation, javascript référence Mozilla
+
+let =  variable local
+var = variable global
+console.log(2+2);
+*/
+//  rechercher = Ctrl + f
+//  = ctrl+/
+// ctrl+B : Ranger la barre de tâche
+//  deplacer la ligne alt+fleche en haut
+/*
+function dieBonjour(){
+    alert("bonjour toi");
+    alert("je suis Konan comment tu vas ?");
+    alert("je suis kady");
+}
+dieBonjour()
+// dieBonjour()
+*/
+/*
+function afficheNom(nom, prenom){ 
+   console.log(nom+ prenom);
+}
+
+afficheNom(prompt("afficher prenom"));
+*/
+
+// function  DemandeAge ( age=30){
+//     alert("quel age à tue ?"+ age ); 
+//     alert("tu as "+ age + "ans" ); 
+
+// }
+
+// DemandeAge(30)
+
+// function Prevoirage(){
+//     let age = prompt("Quel age à tue ");
+//     // age=parseInt(age);
+//      age = Number(age);
+//     alert ("Bientot vous aurez " + (age+1) + "ans.");    
+// }
+
+// Prevoirage();
+
